ae8891ededca16aa4cd549f4a03ad739faae597f
Daymer Perdomo <daimer003@gmail.com>
2025-10-27 22:07:18 -0500

    feat: text

---
+++ src/components/ui/Subtitle.vue
@@ -0,0 +1,45 @@
+<template>
+  <h2
+    :class="[
+      'font-semibold leading-snug text-center',
+      sizeClass,
+      colorClass,
+    ]"
+  >
+    <slot />
+  </h2>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+  size: {
+    type: String,
+    default: 'md', // sm, md, lg
+  },
+  color: {
+    type: String,
+    default: 'white',
+  },
+})
+
+const sizeClass = computed(() => {
+  switch (props.size) {
+    case 'sm':
+      return 'text-lg md:text-xl'
+    case 'md':
+      return 'text-2xl md:text-3xl'
+    default:
+      return 'text-3xl md:text-4xl'
+  }
+})
+
+const colorClass = computed(() => {
+  return props.color === 'black'
+    ? 'text-gray-900'
+    : props.color === 'gray'
+    ? 'text-gray-400'
+    : 'text-white'
+})
+</script>
---
+++ src/components/ui/Title.vue
@@ -0,0 +1,52 @@
+<template>
+  <h1
+    :class="[
+      'font-extrabold leading-tight text-center',
+      sizeClass,
+      colorClass,
+      uppercase ? 'uppercase' : '',
+    ]"
+  >
+    <slot />
+  </h1>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+  size: {
+    type: String,
+    default: 'xl', // sm, md, lg, xl
+  },
+  color: {
+    type: String,
+    default: 'white', // puedes pasar 'black' o 'gray'
+  },
+  uppercase: {
+    type: Boolean,
+    default: false,
+  },
+})
+
+const sizeClass = computed(() => {
+  switch (props.size) {
+    case 'sm':
+      return 'text-xl md:text-2xl'
+    case 'md':
+      return 'text-3xl md:text-4xl'
+    case 'lg':
+      return 'text-4xl md:text-5xl'
+    default:
+      return 'text-5xl md:text-6xl'
+  }
+})
+
+const colorClass = computed(() => {
+  return props.color === 'black'
+    ? 'text-gray-900'
+    : props.color === 'gray'
+    ? 'text-gray-500'
+    : 'text-white'
+})
+</script>
